ea0d1a7e83938a8b75a788a255341fdb3742cc43
vilagen <vilagen@yahoo.com>
2019-09-01 10:23:34 -0400

    added a box in the container cards are stored in so the layout is more even.

--- memorygame/src/components/CardWrapper/index.js
+++ memorygame/src/components/CardWrapper/index.js
@@ -3,7 +3,7 @@ import "./styleCardWrapper.css"
 
 function CardWrapper(props) {
     return (
-        <div className="container my-3 border">
+        <div className="my-3 border container box">
             <div className="row col-12-md">
                 <div className="d-flex justify-content-between card-rows">
                     {props.children}
